6fb1fc12cf5fc96fdd7e7f192865576e062f0bf6
Arnik <arnikghosh15@gmail.com>
2020-04-28 23:28:25 +0530

    add sum method

--- Hello.java
+++ Hello.java
@@ -4,10 +4,15 @@ class Hello{
     {
         System.out.println("Hello " + msg);
     }
+    public void sum(int n1,int n2)
+    {
+        System.out.println("Total Sum " + (n1 + n2));
+    }
     public static void main(String[] args)
     {
         System.out.println("Hello Wolrd");
         Hello obj = new Hello();
         obj.greetings("Git");
+        obj.sum(5,10);
     }
 }
