a737db3f883169a48e03026eb5d7f36f770946e8
Arnik <arnikghosh15@gmail.com>
2020-04-28 22:01:59 +0530

    Add a new method call greetings

--- Hello.java
+++ Hello.java
@@ -1,6 +1,13 @@
 class Hello{
+
+    public void greetings(String msg)
+    {
+        System.out.println("Hello " + msg);
+    }
     public static void main(String[] args)
     {
         System.out.println("Hello Wolrd");
+        Hello obj = new Hello();
+        obj.greetings("Git");
     }
 }
