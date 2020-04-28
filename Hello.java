9f24c98b31a23e5e7d539c83c602bacdcf8d96fa
Arnik <arnikghosh15@gmail.com>
2020-04-29 00:15:47 +0530

    add message function

--- Hello.java
+++ Hello.java
@@ -1,17 +1,7 @@
 class Hello{
 
-    public void greetings(String msg)
+    public void message(String msg)
     {
-        System.out.println("Bye Bye" + msg);
-    }
-    public void sum(int n1,int n2)
-    {
-        System.out.println("Total Sum " + (n1 + n2));
-    }
-    public static void main(String[] args)
-    {
-        System.out.println("Hello Wolrd");
-        Hello obj = new Hello();
-        obj.greetings("Git Lab");
+        System.out.println("Hello Message***");
     }
 }
